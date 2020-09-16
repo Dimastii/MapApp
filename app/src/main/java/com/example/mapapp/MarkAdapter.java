bea18f2ab605f2d6069fe30d231efc618771894a
Dimastii <Dimastii100500@yandex.ru>
2020-09-16 13:37:07 +0400

    add item to rcycler to sliding pannel

--- app/src/main/java/com/example/mapapp/MarkAdapter.java
+++ app/src/main/java/com/example/mapapp/MarkAdapter.java
@@ -1,35 +1,55 @@
 package com.example.mapapp;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+
 public class MarkAdapter extends RecyclerView.Adapter<MarkAdapter.MarkViewHolder> {
 
+    private final List<Token> marks;
 
+    public MarkAdapter(List<Token> marks) {
+        this.marks = marks;
+    }
 
     @NonNull
     @Override
     public MarkViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.move_item_in_slide_pannel, parent, false);
+        return new MarkViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MarkViewHolder holder, int position) {
-
+        holder.bind(marks.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return marks.size();
     }
 
     static final class MarkViewHolder extends RecyclerView.ViewHolder{
 
+        private  final TextView keyTextView;
+        private  final TextView titleTextView;
+
         public MarkViewHolder(@NonNull View itemView) {
             super(itemView);
+            keyTextView = itemView.findViewById(R.id.move_item_in_slide_pannel_key);
+            titleTextView = itemView.findViewById(R.id.move_item_in_slide_pannel_title);
+        }
+        private void bind(@NonNull Token Mark){
+            keyTextView.setText(Mark.key);
+            titleTextView.setText(Mark.title);
         }
     }
+
 }
